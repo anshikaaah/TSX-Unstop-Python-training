34b9808f19146fdf42a6cf93da12d89ba766838a
anshikaaah <123300211+anshikaaah@users.noreply.github.com>
2025-05-17 15:45:19 +0530

    Week 1 committed

---
+++ Week 1/task1.py
@@ -0,0 +1,6 @@
+l=int(input("Enter length of the rectangle"))
+# takes length as input from user
+b=int(input("Enter breadth of the rectangle"))
+# takes breadth as input from user
+area=l*b
+print("Area of the rectangle is: ", area)
